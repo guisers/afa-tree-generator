962e8b4af001d34eecf1bab27798dd5bdfdadaf6
Crystal To <crystal.kyto@gmail.com>
2016-11-05 19:41:09 -0700

    Adds: Upload .csv file, parse csv to json, display results

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,47 @@ const testData = require('../test/data') // FOR TESTING
 var express = require('express')
 var router = express.Router()
 
-/* GET landing page. */
-router.get('/', function(req, res, next) {
+// File upload utils
+var multer  = require('multer')
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
+
+// csv to json utils
+var Converter = require('csvtojson').Converter;
+
+const indexRender = function(req, res, next) {
   res.render('index', {
     title: 'AFA Christmas Tree Generator'
   })
-})
+}
+
+const resultRender = function(req, res, next) {
+  console.log('Generating tree with the following data:')
+  console.log(res.locals.donors)
+  res.render('result', {
+    title: 'Generated Tree'
+  })
+}
+
+const fileParser = function(req, res, next) {
+  const file = req.file
+  const csvString = file.buffer.toString()
+  const converter = new Converter({});
+  converter.fromString(csvString, function(err, result) {
+    res.locals.donors = result
+    return next()
+  })
+}
+
+/* GET landing page. */
+router.get('/', indexRender)
 
 /* POST generate a tree. */
-router.post('/generate', function(req, res, next) {
-  res.redirect('/result')
-})
+router.post('/generate', upload.single('donorData'), fileParser, resultRender)
 
-/* GET result page. */
-router.get('/result', function(req, res, next) {
-  res.render('result', {
-    title: 'Generated Tree',
-    donors: testData.donors
-  })
+/* GET generate page. */
+router.get('/generate', function(req, res, next) {
+  res.redirect('/')
 })
 
 module.exports = router
